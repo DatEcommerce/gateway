c8a4c28bd2a5095354795deb0e7d42fb3358202a
Đạt <datnguyen101004@gmail.com>
2025-09-04 20:28:18 +0700

    add rate limited + retry for gateway

--- src/main/java/com/dat/backend/gateway/config/RouteConfig.java
+++ src/main/java/com/dat/backend/gateway/config/RouteConfig.java
@@ -3,18 +3,22 @@ package com.dat.backend.gateway.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import java.net.URI;
+import java.time.Duration;
+import java.util.Set;
 
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.fallbackHeaders;
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.*;
+import static org.springframework.cloud.gateway.server.mvc.filter.Bucket4jFilterFunctions.rateLimit;
 import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
-import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.filter.RetryFilterFunctions.retry;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.adaptCachedBody;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,12 +37,22 @@ public class RouteConfig {
                             .setFallbackUri(URI.create("forward:/fallback/products"))
                             .setStatusCodes("500", "404", "503");
                 }))
-                //.filter(lb("product-service"))
+                .filter(rateLimit(c->c
+                        .setCapacity(100)
+                        .setPeriod(Duration.ofMinutes(1))
+                        .setKeyResolver(request -> request.servletRequest().getUserPrincipal().getName())))
+                .filter(retry(c -> c
+                        .setRetries(3)
+                        .setSeries(Set.of(HttpStatus.Series.SERVER_ERROR))
+//                        .setExceptions(Set.of(AuthenticationException.class))
+                        .setCacheBody(true)))
+                .filter(adaptCachedBody())
+//                .filter(lb("product-service"))
                 .build()
                 .and(route("forward_route")
                         .route(path("/fallback/products"), http())
                         .before(uri("http://localhost:8082/fallback/products")) // route to fallback service
-                        .before(fallbackHeaders()) // add fallback headers
+                        .before(fallbackHeaders()) // add headers to fallback service
                         .build());
     }
 }
